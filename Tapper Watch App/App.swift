c700e4e031adfa446f30878898a690a07cf4d0f4
Ryan F <forsyth.r@gmail.com>
2024-11-16 16:31:29 +0000

    👼 Project: empty iOS + watchOS targets
    - Set minimum iOS to 17
    - Set minimum watchOS to 11
    - Add gitignore

---
+++ Tapper Watch App/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  Tapper Watch App
+//
+//  Created by Ryan on 2024-11-16.
+//
+
+import SwiftUI
+
+@main
+struct Tapper_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tapper/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  Tapper
+//
+//  Created by Ryan on 2024-11-16.
+//
+
+import SwiftUI
+
+@main
+struct TapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

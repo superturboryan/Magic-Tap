a9ce7cdda321177de16da984ee053aa80c6ea906
Ryan F <forsyth.r@gmail.com>
2024-11-17 08:13:06 +0000

    🚰 PhoneView: Subscribe to action notification
    - Actions are printed for debugging

--- Tapper/PhoneView.swift
+++ Tapper/PhoneView.swift
@@ -12,6 +12,10 @@ struct PhoneView: View {
     
     @EnvironmentObject var wcManager: WCManager
     
+    let actionPublisher = NotificationCenter.default.publisher(
+        for: Action.key.notification
+    )
+    
     var body: some View {
         VStack {
             if wcManager.isReachable {
@@ -25,6 +29,38 @@ struct PhoneView: View {
         }
         .imageScale(.large)
         .padding()
+        .onReceive(actionPublisher) { handleNotifcation($0) }
+    }
+    
+    func handleNotifcation(_ notification: Notification) {
+        guard let action = notification.userInfo?[Action.key] as? Action else {
+            print("Action not found in notification.userInfo")
+            return
+        }
+        perform(action)
+    }
+    
+    func perform(_ action: Action) {
+        print("Performing \(action)")
+        switch action {
+
+        case .incrementVolume:
+            print("Not implemented")
+        case .decrementVolume:
+            print("Not implemented")
+        case .toggleMute:
+            print("Not implemented")
+        case .togglePlayPause:
+            print("Not implemented")
+        case .nextTrack:
+            print("Not implemented")
+        case .previousTrack:
+            print("Not implemented")
+        case .vibrate:
+            print("Not implemented")
+        case .none:
+            print("Not implemented")
+        }
     }
 }
 

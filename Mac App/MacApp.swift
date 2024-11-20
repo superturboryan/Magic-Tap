7ee27d359e8fc74d5ae7e3d5858925f367fd830f
Ryan F <forsyth.r@gmail.com>
2024-11-20 19:18:14 +0000

    💻 Project: Add empty macOS target
    - Remove boilerplate files
    - Set minimum macOS to 14.6 (Sonoma)
    - Add Common package as dependency
    - Add AppIcon assets for size 16x16, 32x32, 64x64, 128x128, 256x256, 512x512
    - TODO: AppIcon should have corners removed, macOS apps do not adjust assets

---
+++ Mac App/MacApp.swift
@@ -0,0 +1,18 @@
+//
+//  MacApp.swift
+//  Magic Tap Mac App
+//
+//  Created by Ryan on 2024-11-20.
+//
+
+import Common
+import SwiftUI
+
+@main
+struct Magic_Tap_Mac_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MacView()
+        }
+    }
+}
---
+++ Mac App/MacView.swift
@@ -0,0 +1,24 @@
+//
+//  MacView.swift
+//  Magic Tap Mac App
+//
+//  Created by Ryan on 2024-11-20.
+//
+
+import SwiftUI
+
+struct MacView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MacView()
+}
